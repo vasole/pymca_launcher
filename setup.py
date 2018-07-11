4f42671e46c95444195f9b14c5352efdedfabc85
V. Armando Solé <sole@esrf.fr>
2018-07-11 18:32:41 +0200

    Change install requires.

--- setup.py
+++ setup.py
@@ -20,14 +20,14 @@
 
 __authors__ = ["V.A. Sole"]
 __license__ = "MIT"
-__date__ = "27/04/2018"
+__date__ = "11/07/2018"
 
 import glob
 import os
 from setuptools import setup
 
 
-version = "5.3.1"
+version = "5.3.2"
 name = "pymca"
 author = "ESRF - Data Analysis Unit"
 description = "pymca"
@@ -52,11 +52,19 @@ classifiers = ["Development Status :: 5 - Production/Stable",
                "Programming Language :: Python :: 2.7",
                "Programming Language :: Python :: 3.5",
                "Programming Language :: Python :: 3.6",
+               "Programming Language :: Python :: 3.7",
                "Topic :: Scientific/Engineering :: Chemistry",
                "Topic :: Scientific/Engineering :: Physics",
                "Topic :: Software Development :: Libraries :: Python Modules",
                ]
-install_requires = ["PyMca5>=5.3.1"]
+install_requires = ["PyMca5>=5.3.2",
+                    "silx>=0.8.0",
+                    "h5py",
+                    "numpy",
+                    "PyOpenGL",
+                    "PyQt5",
+                    "matplotlib"]
+                    #"qtconsole Not sure about its availability
 
 if __name__ == "__main__":
     setup(name=name,
