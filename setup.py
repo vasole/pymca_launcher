add508d63f0a89348a0b488c979dadd84e10988a
V. Armando Solé <sole@esrf.fr>
2020-01-13 13:35:27 +0100

    Update version.

--- setup.py
+++ setup.py
@@ -20,14 +20,14 @@
 
 __authors__ = ["V.A. Sole"]
 __license__ = "MIT"
-__date__ = "21/10/2019"
+__date__ = "10/01/2020"
 
 import glob
 import os
 from setuptools import setup
 
 
-version = "5.5.3"
+version = "5.5.4"
 name = "pymca"
 author = "ESRF - Data Analysis Unit"
 description = "pymca"
@@ -57,7 +57,7 @@ classifiers = ["Development Status :: 5 - Production/Stable",
                "Topic :: Scientific/Engineering :: Physics",
                "Topic :: Software Development :: Libraries :: Python Modules",
                ]
-install_requires = ["PyMca5>=5.5.3",
+install_requires = ["PyMca5>=5.5.4",
                     "h5py",
                     "numpy",
                     "PyOpenGL",
